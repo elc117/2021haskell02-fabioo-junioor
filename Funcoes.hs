c8b39a4e964a4ca2bddd4ad6212eeabb90bb7085
FabioJr <fcjunior@inf.ufsm.br>
2021-05-26 18:56:00 +0000

    Segundo commit

---
+++ Funcoes.hs
@@ -0,0 +1,7 @@
+module Funcoes
+(
+febre
+)where
+
+febre :: Float -> Bool
+febre f1 = if f1 > 38.80 then True else False
--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,8 @@
 -- Prática 02 de Haskell
 -- Nome: Fabio Junior
+
+import Funcoes
+
+{-1 Médicos consideram que um indivíduo tem febre quando sua temperatura corpórea está acima de 37,8oC. Escreva uma função comFebre :: [Float] -> [Float] que, dada uma lista de temperaturas de indivíduos, selecione aquelas que representam febre. Resolva esta questão definindo uma função auxiliar nomeada, que verifica se uma dada temperatura é febre ou não.-}
+comFebre :: [Float] -> [Float]
+comFebre f = filter febre f
