193ff43d7added1ced7a2dac3206e7b0f5c66956
FabioJr <fcjunior@inf.ufsm.br>
2021-05-29 22:49:35 +0000

    Terceiro commit

--- haskell02.hs
+++ haskell02.hs
@@ -22,8 +22,22 @@ itemize str = map (\x -> "<li>"++ x ++"</li>") str
 bigCircles :: Float -> [Float] -> [Float]
 bigCircles num raios = filter ((\x y -> x < (3.14* y^2)) num) raios
 
+
 {-5 Escreva uma função quarentena :: [(String,Float)] -> [(String,Float)] que receba uma lista de tuplas com nomes de pessoas e suas temperaturas corpóreas, e selecione aquelas que têm febre.-}
 quarentena :: [(String,Float)] -> [(String,Float)]
 quarentena pessoa = filter (\(_,t) -> (febre t)) pessoa
 
+
 {-6 Escreva uma função idadesEm :: [Int] -> Int -> [Int] que receba uma lista de anos de nascimento de algumas pessoas e um ano de referência. A lista resultante terá idades calculadas considerando o ano de referência (idades aproximadas, já que só consideram o ano, não a data completa de nascimento). Resolva esta questão usando lambda.-}
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm lisAnos aRef = map (\x -> if anoRef aRef x then aRef - x else 0) lisAnos
+
+
+{-7 Escreva uma função changeNames :: [String] -> [String] que receba uma lista de nomes e adicione o prefixo "Super " às strings que começarem com a letra A (maiúscula), deixando as demais inalteradas. A lista resultante, portanto, terá a mesma quantidade de strings da lista original.-}
+changeNames :: [String] -> [String]
+changeNames lisNomes = map (\x -> if head x == 'A' then "Super "++ x else x) lisNomes
+
+
+{-8 Escreva uma função onlyShorts :: [String] -> [String] que receba uma lista de strings e retorne outra lista contendo somente as strings cujo tamanho seja menor que 5.-}
+onlyShorts :: [String] -> [String]
+onlyShorts listStr = filter (\x -> length x < 5) listStr
